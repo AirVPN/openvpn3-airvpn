ad39afdb1547b4b056a55ada6d3d4628e4be77f0
promind <promindsd@gmail.com>
2025-07-31 12:38:39 +0200

    Updated Android.mk to new OpenVPN3 requirements and C++20 specifications

--- Android.mk
+++ Android.mk
@@ -10,16 +10,16 @@ ifeq ($(SSL_LIBRARY), mbedTLS)
     LOCAL_C_INCLUDES := lzo/include lz4/lib mbedtls mbedtls/include openvpn3-airvpn/client openvpn3-airvpn asio/asio/include
 else ifeq ($(SSL_LIBRARY), openSSL)
     LOCAL_STATIC_LIBRARIES := liblzo_static liblz4_static libssl_static libcrypto_static
-    LOCAL_CFLAGS := -funwind-tables -DHAVE_CONFIG_H -DTARGET_ABI=\"${TARGET_ABI}\" -DUSE_OPENSSL=1 -DOPENSSL_NO_ENGINE -DHAVE_LZO -DHAVE_LZ4 -DUSE_ASIO
+    LOCAL_CFLAGS := -Wno-deprecated-declarations -funwind-tables -DHAVE_CONFIG_H -DTARGET_ABI=\"${TARGET_ABI}\" -DUSE_OPENSSL=1 -DHAVE_LZO -DHAVE_LZ4 -DUSE_ASIO
     LOCAL_C_INCLUDES := lzo/include lz4/lib openssl openssl/include openssl/crypto openssl/crypto/include openvpn3-airvpn/client openvpn3-airvpn asio/asio/include
 endif
 
-LOCAL_CXXFLAGS += -std=c++17
+LOCAL_CXXFLAGS += -std=c++20 -include openvpn/log/logbasesimple.hpp
 
 LOCAL_CPP_FEATURES += exceptions rtti
 
 LOCAL_MODULE := openvpn3
 
-LOCAL_SRC_FILES := client/ovpncli.cpp
+LOCAL_SRC_FILES := client/ovpncli.cpp openvpn/crypto/data_epoch.cpp
 
 include $(BUILD_SHARED_LIBRARY)
