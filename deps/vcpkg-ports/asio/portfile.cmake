7f572f4fe647a36f5a1094cbeb261a5bcdae5047
Frank Lichtenheld <frank@lichtenheld.com>
2026-08-27 14:16:43 +0200

    asio: Rebase port and patchset on 1.38.2
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- deps/vcpkg-ports/asio/portfile.cmake
+++ deps/vcpkg-ports/asio/portfile.cmake
@@ -5,7 +5,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO chriskohlhoff/asio
     REF "${ref}"
-    SHA512 d44b35d9d1900de35aa10bf339c7e16a06e110377fd70fbefba91599d24cff32cc3dc88a4b0bf1e1706f9ac46177982edb5c7f969b72a57123be6550a3b062d8
+    SHA512 d340396e320f352a860c9f4904457a365969a95e2e564675e024a3a5885194382f37708a87505904a99688a2f1fd3f1abe64959316bf7e73032d3250bc3b76be
     HEAD_REF master
     PATCHES
         0001-Added-Apple-NAT64-support-when-both-ASIO_HAS_GETADDR.patch
@@ -18,7 +18,7 @@ vcpkg_from_github(
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 
 # Always use "ASIO_STANDALONE" to avoid boost dependency
-vcpkg_replace_string("${SOURCE_PATH}/asio/include/asio/detail/config.hpp" "defined(ASIO_STANDALONE)" "!defined(VCPKG_DISABLE_ASIO_STANDALONE)")
+vcpkg_replace_string("${SOURCE_PATH}/include/asio/detail/config.hpp" "defined(ASIO_STANDALONE)" "!defined(VCPKG_DISABLE_ASIO_STANDALONE)")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
@@ -27,8 +27,8 @@ vcpkg_cmake_configure(
 )
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
-    
+
 vcpkg_cmake_config_fixup()
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/asio-config.cmake" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
 
-vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/asio/LICENSE_1_0.txt")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE_1_0.txt")
